2ed3e289b2bf1b997bb2e3a8790cbb5e81db0603
Alberto Pirillo <alberto.pirillo@mail.polimi.it>
2021-04-22 20:25:45 +0200

    Added initial parameters

--- project/src/param.cpp
+++ project/src/param.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include <dynamic_reconfigure/server.h>
 #include <project/parametersConfig.h>
+#include <std_msgs/Float64.h>
 
 void callback(project::parametersConfig &config, uint32_t bitmask_level)
 {
@@ -8,13 +9,18 @@ void callback(project::parametersConfig &config, uint32_t bitmask_level)
     ROS_INFO ("%d",bitmask_level);
 }
 
-
 //TODO: initialize parameter into launch file
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "param");
+    ros::NodeHandle n;
 
-    //Create server and specify callback function type
+    //Publish initial parameters (no dynamic reconfigure needed)
+    ros::Publisher pub_x = n.advertise<std_msgs::Float64>("initial_x", 1000);
+    ros::Publisher pub_y = n.advertise<std_msgs::Float64>("initial_y", 1000);
+    ros::Publisher pub_theta = n.advertise<std_msgs::Float64>("initial_theta", 1000);
+
+    //Create the server and specify callback function type
     dynamic_reconfigure::Server<project::parametersConfig> server;
     dynamic_reconfigure::Server<project::parametersConfig>::CallbackType f;
 
--- project/src/pub_odom.cpp
+++ project/src/pub_odom.cpp
@@ -1,17 +1,38 @@
 #include "ros/ros.h"
 #include "robotics_hw1/MotorSpeed.h"
 #include "project/Odom_and_method.h"
+#include "data.h"
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <math.h>
-#include "data.h"
+#include <std_msgs/Float64.h>
+
+//Global scope
+enum method {Euler, Runge_Kutta};
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "odom");
     ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<project::Odom_and_method>("my_odom", 1000);
+    ros::Publisher pub_odom = n.advertise<project::Odom_and_method>("my_odom", 1000);
+
+    //Get initial parameters
+    int current_method;
+    float initial_x, initial_y, initial_theta;
+    n.getParam("/initial_x", initial_x); 
+    n.getParam("/initial_y", initial_y); 
+    n.getParam("/initial_theta", initial_theta); 
+    n.getParam("/param_method", current_method);
+
+    while (ros::ok()) 
+    {
+        n.getParam("/param_method", current_method);
+        if (current_method == method::Euler) ;//Euler
+        else ;//Runge-Kutta
+
+        //TODO: spin?
+    }
 
     return 0;
 }
